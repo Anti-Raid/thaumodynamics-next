6742def173483516bc75a117a213e6e5fae87ef1
Miya25 <foenx0315@gmail.com>
2025-07-15 22:32:01 +0530

    thing

--- src/app/layout.config.tsx
+++ src/app/layout.config.tsx
@@ -55,7 +55,7 @@ export const baseOptions: BaseLayoutProps = {
           icon: <FaCogs />,
           text: "User Guide",
           description: "How to guide on setting up AntiRaid",
-          url: "/docs/README",
+          url: "/docs/user/about/readme",
         },
         {
           icon: <FaBook />,
