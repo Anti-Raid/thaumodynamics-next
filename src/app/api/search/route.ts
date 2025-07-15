8fbf6a5dfeb08ab9a49921cce394e9467911e276
Miya25 <foenx0315@gmail.com>
2025-07-15 20:54:25 +0530

    thing

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -1,18 +1,4 @@
-import { createSearchAPI } from "fumadocs-core/search/server";
 import { source } from "@/lib/source";
+import { createFromSource } from "fumadocs-core/search/server";
 
-export const { GET } = createSearchAPI("advanced", {
-  indexes: await Promise.all(
-    source.getPages().map(async (page) => {
-      const { structuredData } = await page.data.load();
-
-      return {
-        title: page.data.title,
-        description: page.data.description,
-        url: page.url,
-        id: page.url,
-        structuredData,
-      };
-    }),
-  ),
-});
+export const { GET } = createFromSource(source);
